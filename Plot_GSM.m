dcfdfe6f5175526b3a7798b35b43dc74ddf67f17
tworitdash <tworitdash@gmail.com>
2019-12-13 17:22:06 +0100

    Changes in office

--- Plot_GSM.m
+++ Plot_GSM.m
@@ -1,14 +1,14 @@
 %close all;
-<<<<<<< HEAD
+%<<<<<<< HEAD
 % c_ = load('TE_TE_Spp.mat');
-c_a = load('TE_TE_Srr_analytical_3.mat');
-=======
+c_a = load('TE_TE_Srp_analytical_3.mat');
+%=======
 c_ = load('TE_TE_Srp.mat');
-c_a = load('TE_TE_Spp_analytical_2.mat');
->>>>>>> 0a21b496e0b0f28d61b4a47fcc24420c9c551183
+%c_a = load('TE_TE_Spp_analytical_2.mat');
+%>>>>>>> 0a21b496e0b0f28d61b4a47fcc24420c9c551183
 %c_a = load('TE_TE_S11_p_n.mat');
 % GSM_ = c_.Srp;
-GSM_a = c_a.Srr;
+GSM_a = c_a.Srp;
 
 % F = 1e9:1e9:100e9;
 F_ = 4e9:0.5e9:30e9;
@@ -26,11 +26,11 @@ hold on;
 hold on;
 % plot(F * 1e-9, db((GSM_a(:, i, i))/max(GSM_a(:, i, i)))/2, 'LineWidth', 2); grid on;
 
-<<<<<<< HEAD
+%<<<<<<< HEAD
 plot(F * 1e-9, db(abs(GSM_a(:, i, i)))/2, 'LineWidth', 2); grid on;
-=======
-plot(F * 1e-9, (real(GSM_a(:, i, i))), 'LineWidth', 2); grid on;
->>>>>>> 0a21b496e0b0f28d61b4a47fcc24420c9c551183
+%=======
+%plot(F * 1e-9, (real(GSM_a(:, i, i))), 'LineWidth', 2); grid on;
+%>>>>>>> 0a21b496e0b0f28d61b4a47fcc24420c9c551183
 
 xlabel('Frequency (GHz)', 'FontSize', 12, 'FontWeight', 'bold');
 ylabel('S in  dB', 'FontSize', 12, 'FontWeight', 'bold');
@@ -43,7 +43,7 @@ end
 
 legend({'TE_{11} Numerical', 'TE_{11} Analytical', 'TE_{12} Numerical', 'TE_{12} Analytical', ...
     'TE_{13} Numerical', 'TE_{13} Analytical',}, 'FontSize', 8, 'FontWeight', 'bold', 'location', 'southeast');
-xlim([4 30]);
+% xlim([4 30]);
 
 %% Save 
 
